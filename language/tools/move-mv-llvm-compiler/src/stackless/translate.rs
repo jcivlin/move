0a5d3a2c8b2f2fcd457421804899af671dc19026
Jan Civlin <jan.civlin@solana.com>
2023-05-12 23:27:28 +0000

    051323-add-field-names

--- language/tools/move-mv-llvm-compiler/src/stackless/translate.rs
+++ language/tools/move-mv-llvm-compiler/src/stackless/translate.rs
@@ -606,11 +606,76 @@ impl<'mm, 'up> FunctionContext<'mm, 'up> {
             self.llvm_builder.position_at_end(entry_block);
         }
 
+        ////////////////////////////////////////////////////////////////////////
+        // Collect some local names from various structure field references.
+        let mut named_locals = BTreeMap::new();
+        for instr in &fn_data.code {
+            use sbc::Operation;
+            match instr {
+                sbc::Bytecode::Call(_, dst, op, src, None) => {
+                    match op {
+                        Operation::BorrowField(mod_id, struct_id, _types, offset) => {
+                            assert_eq!(src.len(), 1);
+                            assert_eq!(dst.len(), 1);
+                            let senv = self
+                                .get_global_env()
+                                .get_module(*mod_id)
+                                .into_struct(*struct_id);
+                            let tmp_idx = dst[0];
+                            let fenv = senv.get_field_by_offset(*offset);
+                            let name = fenv.get_name().display(senv.symbol_pool()).to_string();
+                            let n = format!("{}", name);
+                            named_locals.insert(tmp_idx, n);
+                        }
+                        Operation::Pack(mod_id, struct_id, _types) => {
+                            let senv = self
+                                .get_global_env()
+                                .get_module(*mod_id)
+                                .into_struct(*struct_id);
+                            assert_eq!(dst.len(), 1);
+                            assert_eq!(src.len(), senv.get_field_count());
+                            let mut offset = 0;
+                            for tmp_idx in src {
+                                let fenv = senv.get_field_by_offset(offset);
+                                let name = fenv.get_name().display(senv.symbol_pool()).to_string();
+                                let n = format!("{}", name);
+                                named_locals.insert(*tmp_idx, n);
+                                offset = offset + 1;
+                            }
+                        }
+                        Operation::Unpack(mod_id, struct_id, _types) => {
+                            let senv = self
+                                .get_global_env()
+                                .get_module(*mod_id)
+                                .into_struct(*struct_id);
+                            assert_eq!(src.len(), 1);
+                            assert_eq!(dst.len(), senv.get_field_count());
+                            let mut offset = 0;
+                            for tmp_idx in dst {
+                                let fenv = senv.get_field_by_offset(offset);
+                                let name = fenv.get_name().display(senv.symbol_pool()).to_string();
+                                let n = format!("{}", name);
+                                named_locals.insert(*tmp_idx, n);
+                                offset = offset + 1;
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        }
+        dbg!(&named_locals);
+        ////////////////////////////////////////////////////////////////////////
+
         // Declare all the locals as allocas
         {
             for (i, mty) in fn_data.local_types.iter().enumerate() {
                 let llty = self.llvm_type(mty);
-                let name = format!("local_{}", i);
+                let mut name = format!("local_{}", i);
+                if let Some(s) = &named_locals.get(&i) {
+                    name = s.to_string();
+                }
                 let llval = self.llvm_builder.build_alloca(llty, &name);
                 self.locals.push(Local {
                     mty: mty.clone(), // fixme bad clone
