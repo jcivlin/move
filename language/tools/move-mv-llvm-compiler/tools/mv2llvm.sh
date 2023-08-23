923372bc8074b03c199e5eadfaf0bb1449f24006
Jan Civlin <jan.civlin@solana.com>
2023-08-23 00:01:47 +0000

    082223-add-mv2llvm-script: added copyright

--- language/tools/move-mv-llvm-compiler/tools/mv2llvm.sh
+++ language/tools/move-mv-llvm-compiler/tools/mv2llvm.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) The Diem Core Contributors
+# Copyright (c) The Move Contributors
+# SPDX-License-Identifier: Apache-2.0
 
 ### Use this script to run all .mv files in a given directory thru `move-mv-llvm-compiler``.
 ###
